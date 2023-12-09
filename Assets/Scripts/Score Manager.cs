f577cd34cb8eb29a2cc31d85c4a2e4c9cd1f791d
Hadhemi33 <benmansourhadhemi01@gmail.com>
2023-12-09 16:38:16 +0100

    Game Over

--- Assets/Scripts/Score Manager.cs
+++ Assets/Scripts/Score Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
@@ -9,14 +10,25 @@ public class ScoreManager : MonoBehaviour
     private int player2Score = 0;
     public Text player1ScoreText;
     public Text player2ScoreText;
+    public int scoreToReach;
     public void Player1Goal()
     {
         player1Score++;
         player1ScoreText.text = player1Score.ToString();
+        CheckScore();
     }
     public void Player2Goal()
     {
         player2Score++;
         player2ScoreText.text = player2Score.ToString();
+        CheckScore();
+
+    }
+    private void CheckScore()
+    {
+        if (player1Score == scoreToReach || player2Score == scoreToReach)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
